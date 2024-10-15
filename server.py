c6086dbd1f13211dd5892bbe4f6ab8e65f659e4e
ezfruit <ezhang8@buffalo.edu>
2024-10-14 22:51:48 -0400

    Login Backend Made

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
 from flask import Flask,request,redirect,url_for # type: ignore
 from flask import render_template  # type: ignore
+from flask import make_response # type: ignore
 from flask_bootstrap5 import Bootstrap # type: ignore
 from pymongo import MongoClient #database
-from flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt # type: ignore
+import secrets, hashlib
 # Create a flask instance
 
 #database set up
 mongo_client = MongoClient('mongo')
 db = mongo_client["users_info"]
 users = db["users"]
+auth = db["auth"]
 
 
 app = Flask(__name__)
@@ -42,16 +45,37 @@ def register():
             return (render_template("html/register.html",too_long=True))
         #check if passwords match, if they do we proceed to store passwords in database
         if password == confirm_passowrd:
-         hash_password =  bcrypt.generate_password_hash(password).decode('utf-8')
-         user = {'username': username, 'password': hash_password}
-         users.insert_one(user)
-         return redirect(url_for('login'))
+            hash_password = bcrypt.generate_password_hash(password).decode('utf-8')
+            user = {'username': username, 'password': hash_password}
+            users.insert_one(user)
+            return redirect(url_for('login'))
         else:
             return render_template("html/register.html",error=True)
     return render_template("html/register.html", title = "Register")
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = False
+    loggedIn = False
+    if request.method == 'POST':
+        username = request.form["username"]
+        password = request.form["password"]
+        found_user = users.find_one({"username": username})
+        if found_user:
+            hashed_password = found_user["password"]
+            #Check if the submitted password matches the one stored in the database
+            if bcrypt.check_password_hash(hashed_password, password):
+                response = make_response(render_template("html/index.html", title = "Home", loggedIn=True))
+                auth_token = secrets.token_hex(20) #Generate the Authentication Token
+                hasher = hashlib.sha256()
+                bytes = auth_token.encode()
+                hasher.update(bytes)
+                hex_hash = hasher.hexdigest() #Hashed Auth Token as a hexadecimal string
+                token = {hex_hash: username} 
+                auth.insert_one(token) #Store the Hashed Auth Token in a separate database along with its associated user
+                response.set_cookie("auth_token", auth_token, max_age=3600, httponly=True) #Set Auth Token Cookie
+                return response
+        return render_template("html/login.html", error=True)
     return render_template("html/login.html", title = "Login")
 
 # Security issue fixed
