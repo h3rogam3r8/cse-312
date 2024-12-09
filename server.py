bca308f9959d921010cec12ee6e52181c7c1db27
ezfruit <ezhang8@buffalo.edu>
2024-12-09 01:40:10 -0500

    DoS Protection Fixed

--- server.py
+++ server.py
@@ -52,6 +52,10 @@ TIME_WINDOW = 10
 COOLDOWN = 30 
 
 def get_remote_address():
+    if "X-Forwarded-For" in request.headers:
+        return request.headers["X-Forwarded-For"].split(",")[0].strip()
+    elif "X-Real-IP" in request.headers:
+        return request.headers["X-Real-IP"]
     return request.remote_addr
 
 @app.before_request
